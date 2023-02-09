d01cb4ad1ec4e3383040c34f6b3b0db38fbe6879
Jan <jan@ssoukup.com>
2023-02-09 15:12:19 +0100

    🐛 fix(codepath): add missing codePath so we can import it from other modules

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,11 @@
-export { handler as s3Handler } from './s3-handler'
+import path from 'path'
 
+export { handler } from './s3-handler'
 export { S3LambdaStack } from './cdk/stack'
 export { CustomStackProps } from './cdk/types'
-
 import { SetupS3LambdaProps, setupS3Lambda } from './cdk/utils/s3Lambda'
+
+export const codePath = path.resolve(__dirname, './code.zip')
 
 export const CdkUtils = {
 	setupS3Lambda,
